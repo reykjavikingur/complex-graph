7a0deb78e06a4261e61774076a6328fe97a11f06
Andrew Strader <astrader@resourceammirati.com>
2017-11-07 06:20:31 -0500

    show different graphs

--- src/assets/scripts/routes/index.js
+++ src/assets/scripts/routes/index.js
@@ -13,6 +13,17 @@ var template = `
         
         <cx-graph :expr="expr" :radius="radius"></cx-graph>
         
+        <hr/>
+        
+        <cx-graph expr="z" radius="3"></cx-graph>
+        <cx-graph expr="z^2" radius="3"></cx-graph>
+        <br/>
+        <cx-graph expr="(e^(i*pi*0/2) * z)^0.5 / e^(i*pi*0/4)" radius="3"></cx-graph>
+        <cx-graph expr="(e^(i*pi*1/2) * z)^0.5 / e^(i*pi*1/4)" radius="3"></cx-graph>
+        <br/>
+        <cx-graph expr="(e^(i*pi*2/2) * z)^0.5 / e^(i*pi*2/4)" radius="3"></cx-graph>
+        <cx-graph expr="(e^(i*pi*3/2) * z)^0.5 / e^(i*pi*3/4)" radius="3"></cx-graph>
+        
     </main>
     
 </vl-layout>
